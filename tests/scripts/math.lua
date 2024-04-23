26dc08d4e46d63ad490230a89b57e000ac5d57fd
Jengamon <uche.okwo1@gmail.com>
2024-04-23 00:22:57 -0700

    feat: match math.randomseed to PUC-Lua

--- tests/scripts/math.lua
+++ tests/scripts/math.lua
@@ -11,142 +11,142 @@ function is_integer(n)
 end
 
 function test1()
-    return math.abs(  1)     == 1    and
-           math.abs( -1)     == 1    and
-           math.abs(  1.0)   == 1.0  and
-           math.abs( -1.0)   == 1.0  and
-           is_integer(math.abs(1  )) and
-       not is_integer(math.abs(1.0))
+    return math.abs(1) == 1 and
+        math.abs(-1) == 1 and
+        math.abs(1.0) == 1.0 and
+        math.abs(-1.0) == 1.0 and
+        is_integer(math.abs(1)) and
+        not is_integer(math.abs(1.0))
 end
 
 function test2()
     return math.acos(1) == 0.0 and
-           math.abs(math.acos(0)   - math.pi/2) < 1e-7 and
-           math.abs(math.acos(0.5) - math.pi/3) < 1e-7 and
-           math.abs(math.acos(-1)  - math.pi) < 1e-7 and
-           is_nan(math.acos( 2)) and
-           is_nan(math.acos(-2)) and
-       not is_integer(math.acos(1))
+        math.abs(math.acos(0) - math.pi / 2) < 1e-7 and
+        math.abs(math.acos(0.5) - math.pi / 3) < 1e-7 and
+        math.abs(math.acos(-1) - math.pi) < 1e-7 and
+        is_nan(math.acos(2)) and
+        is_nan(math.acos(-2)) and
+        not is_integer(math.acos(1))
 end
 
 function test3()
     return math.asin(0) == 0.0 and
-           math.abs(math.asin(1)   - math.pi/2) < 1e-7 and
-           math.abs(math.asin(0.5) - math.pi/6) < 1e-7 and
-           math.abs(math.asin(-1)  + math.pi/2) < 1e-7 and
-           is_nan(math.asin( 2)) and
-           is_nan(math.asin(-2)) and
-       not is_integer(math.asin(0))
+        math.abs(math.asin(1) - math.pi / 2) < 1e-7 and
+        math.abs(math.asin(0.5) - math.pi / 6) < 1e-7 and
+        math.abs(math.asin(-1) + math.pi / 2) < 1e-7 and
+        is_nan(math.asin(2)) and
+        is_nan(math.asin(-2)) and
+        not is_integer(math.asin(0))
 end
 
 function test4()
     return math.atan(0) == 0.0 and
-           math.abs(math.atan(1)   - math.pi/4) < 1e-7 and
-           math.abs(math.atan(0.5) - 0.46364760900081) < 1e-7 and
-           math.abs(math.atan(-1)  + math.pi/4) < 1e-7 and
-           math.abs(math.atan(2)   - 1.1071487177941) < 1e-7 and
-           math.abs(math.atan(-2)  + 1.1071487177941) < 1e-7 and
-           math.abs(math.atan(math.huge)  - math.pi/2) < 1e-7 and
-           math.abs(math.atan(-math.huge) + math.pi/2) < 1e-7 and
-       not is_integer(math.atan(0))
+        math.abs(math.atan(1) - math.pi / 4) < 1e-7 and
+        math.abs(math.atan(0.5) - 0.46364760900081) < 1e-7 and
+        math.abs(math.atan(-1) + math.pi / 4) < 1e-7 and
+        math.abs(math.atan(2) - 1.1071487177941) < 1e-7 and
+        math.abs(math.atan(-2) + 1.1071487177941) < 1e-7 and
+        math.abs(math.atan(math.huge) - math.pi / 2) < 1e-7 and
+        math.abs(math.atan(-math.huge) + math.pi / 2) < 1e-7 and
+        not is_integer(math.atan(0))
 end
 
 function test5()
     return math.atan(0, 0) == 0.0 and
-           math.atan(0, 1) == 0.0 and
-           math.abs(math.atan( 1,  0) - math.pi/2)  < 1e-7 and
-           math.abs(math.atan( 1,  1) - math.pi/4) < 1e-7 and
-           math.abs(math.atan(-1,  1) + math.pi/4) < 1e-7 and
-           math.abs(math.atan( 1, -1) - 3*math.pi/4)  < 1e-7 and
-           math.abs(math.atan(-1, -1) + 3*math.pi/4)  < 1e-7 and
-           math.abs(math.atan(math.huge, 1) - math.pi/2) < 1e-7 and
-       not is_integer(math.atan(0, 0))
+        math.atan(0, 1) == 0.0 and
+        math.abs(math.atan(1, 0) - math.pi / 2) < 1e-7 and
+        math.abs(math.atan(1, 1) - math.pi / 4) < 1e-7 and
+        math.abs(math.atan(-1, 1) + math.pi / 4) < 1e-7 and
+        math.abs(math.atan(1, -1) - 3 * math.pi / 4) < 1e-7 and
+        math.abs(math.atan(-1, -1) + 3 * math.pi / 4) < 1e-7 and
+        math.abs(math.atan(math.huge, 1) - math.pi / 2) < 1e-7 and
+        not is_integer(math.atan(0, 0))
 end
 
 function test6()
-    return math.ceil( 1.0) ==  1 and
-           math.ceil(-1.0) == -1 and
-           math.ceil( 1.1) ==  2 and
-           math.ceil(-1.1) == -1 and
-           is_integer(math.ceil(1.0))
+    return math.ceil(1.0) == 1 and
+        math.ceil(-1.0) == -1 and
+        math.ceil(1.1) == 2 and
+        math.ceil(-1.1) == -1 and
+        is_integer(math.ceil(1.0))
 end
 
 function test7()
     return math.cos(0) == 1.0 and
-           math.abs(math.cos(math.pi) + 1.0) < 1e-7 and
-           math.abs(math.cos(math.pi/6) - math.sqrt(3)/2) < 1e-7 and
-           math.abs(math.cos(1) - 0.5403023058681398) < 1e-7 and
-           is_nan(math.cos(math.huge)) and
-       not is_integer(math.cos(0))
+        math.abs(math.cos(math.pi) + 1.0) < 1e-7 and
+        math.abs(math.cos(math.pi / 6) - math.sqrt(3) / 2) < 1e-7 and
+        math.abs(math.cos(1) - 0.5403023058681398) < 1e-7 and
+        is_nan(math.cos(math.huge)) and
+        not is_integer(math.cos(0))
 end
 
 function test8()
     return math.abs(math.deg(math.pi) - 180) < 1e-7 and
-           math.abs(math.deg(1) - 180/math.pi) < 1e-7 and
-       not is_integer(math.deg(math.pi))
+        math.abs(math.deg(1) - 180 / math.pi) < 1e-7 and
+        not is_integer(math.deg(math.pi))
 end
 
 function test9()
     return math.abs(math.exp(1) - 2.718281828459) < 1e-7 and
-           math.exp(0) == 1 and
-           math.abs(math.exp(-1) - 0.36787944117144) < 1e-7 and
-           math.exp(-math.huge) == 0 and
-           math.exp(math.huge) == math.huge and
-       not is_integer(math.exp(0))
+        math.exp(0) == 1 and
+        math.abs(math.exp(-1) - 0.36787944117144) < 1e-7 and
+        math.exp(-math.huge) == 0 and
+        math.exp(math.huge) == math.huge and
+        not is_integer(math.exp(0))
 end
 
 function test10()
-    return math.floor( 1.0) ==  1 and
-           math.floor(-1.0) == -1 and
-           math.floor( 1.1) ==  1 and
-           math.floor(-1.1) == -2 and
-           is_integer(math.floor(1.0))
+    return math.floor(1.0) == 1 and
+        math.floor(-1.0) == -1 and
+        math.floor(1.1) == 1 and
+        math.floor(-1.1) == -2 and
+        is_integer(math.floor(1.0))
 end
 
 function test11()
-    return math.abs(math.fmod( 6.2,  3.4) - 2.8) < 1e-7 and
-           math.abs(math.fmod(-6.2,  3.4) + 2.8) < 1e-7 and
-           math.abs(math.fmod( 6.2, -3.4) - 2.8) < 1e-7 and
-           math.abs(math.fmod(-6.2, -3.4) + 2.8) < 1e-7 and
-       not is_integer(math.fmod(1.0, 1.0))
+    return math.abs(math.fmod(6.2, 3.4) - 2.8) < 1e-7 and
+        math.abs(math.fmod(-6.2, 3.4) + 2.8) < 1e-7 and
+        math.abs(math.fmod(6.2, -3.4) - 2.8) < 1e-7 and
+        math.abs(math.fmod(-6.2, -3.4) + 2.8) < 1e-7 and
+        not is_integer(math.fmod(1.0, 1.0))
 end
 
 function test12()
     return math.log(0) == -math.huge and
-           math.log(1) == 0.0 and
-           math.abs(math.log(10) - 2.302585092994) < 1e-7 and
-           is_nan(math.log(-1))
+        math.log(1) == 0.0 and
+        math.abs(math.log(10) - 2.302585092994) < 1e-7 and
+        is_nan(math.log(-1))
 end
 
 function test13()
     return math.log(0, 10) == -math.huge and
-           math.log(1, 10) == 0.0 and
-           math.log(10, 10) == 1.0 and
-           math.log(math.exp(1), math.exp(1)) == 1.0 and
-           math.abs(math.log(3.1622776601684, 10) - 0.5) < 1e-7 and
-           is_nan(math.log(-1, 10))
+        math.log(1, 10) == 0.0 and
+        math.log(10, 10) == 1.0 and
+        math.log(math.exp(1), math.exp(1)) == 1.0 and
+        math.abs(math.log(3.1622776601684, 10) - 0.5) < 1e-7 and
+        is_nan(math.log(-1, 10))
 end
 
 function test14()
     return math.max(1, 2, 3) == 3 and
-           is_integer(math.max(1, 2, 3)) and
-           math.max(1.0, 2.0, 3.0) == 3.0 and
-       not is_integer(math.max(1.0, 2.0, 3.0)) and
-           math.max(3, 3.0, 3.0) == 3 and
-           is_integer(math.max(3, 3.0, 3.0)) and
-           math.max(-5, -4, -3, -2, -1, 0, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1) == 10
+        is_integer(math.max(1, 2, 3)) and
+        math.max(1.0, 2.0, 3.0) == 3.0 and
+        not is_integer(math.max(1.0, 2.0, 3.0)) and
+        math.max(3, 3.0, 3.0) == 3 and
+        is_integer(math.max(3, 3.0, 3.0)) and
+        math.max(-5, -4, -3, -2, -1, 0, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1) == 10
     -- Tests we do not currently pass due to incompatibility with PUC-Rio Lua
     --     is_err(math.max(1, "2", 1))
 end
 
 function test15()
     return math.min(3, 2, 1) == 1 and
-           is_integer(math.min(3, 2, 1)) and
-           math.min(3.0, 2.0, 1.0) == 1.0 and
-       not is_integer(math.min(3.0, 2.0, 1.0)) and
-           math.min(3, 3.0, 3.0) == 3 and
-           is_integer(math.min(3, 3.0, 3.0)) and
-           math.min(5, 4, 3, 2, 1, 0, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1) == -10
+        is_integer(math.min(3, 2, 1)) and
+        math.min(3.0, 2.0, 1.0) == 1.0 and
+        not is_integer(math.min(3.0, 2.0, 1.0)) and
+        math.min(3, 3.0, 3.0) == 3 and
+        is_integer(math.min(3, 3.0, 3.0)) and
+        math.min(5, 4, 3, 2, 1, 0, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1) == -10
     -- Tests we do not currently pass due to incompatibility with PUC-Rio Lua
     --     is_err(math.min(1, "2", 1))
     --     is_nan(math.min(0.0 % 0.0, 1, 2))
@@ -198,23 +198,23 @@ end
 
 function test17()
     return math.abs(math.rad(180) - math.pi) < 1e-7 and
-           math.abs(math.rad(360) - 2*math.pi) < 1e-7 and
-           math.abs(math.rad(15) - math.pi/12) < 1e-7
+        math.abs(math.rad(360) - 2 * math.pi) < 1e-7 and
+        math.abs(math.rad(15) - math.pi / 12) < 1e-7
 end
 
 function test18()
     local good = true
-    for i=1,10000,1 do
+    for i = 1, 10000, 1 do
         local rand = math.random()
         good = good and rand >= 0 and rand < 1
     end
 
-    for i=1,10000,1 do
+    for i = 1, 10000, 1 do
         local rand = math.random(4, 20)
         good = good and rand >= 4 and rand <= 20
     end
 
-    for i=1,10000,1 do
+    for i = 1, 10000, 1 do
         local rand = math.random(35)
         good = good and rand >= 1 and rand <= 35
     end
@@ -224,13 +224,13 @@ function test18()
 
     local numbers1 = {}
     math.randomseed(8675309)
-    for i=1,10000,1 do
+    for i = 1, 10000, 1 do
         numbers1[#numbers1 + 1] = math.random()
     end
 
     local numbers2 = {}
     math.randomseed(8675309, 0)
-    for i=1,10000,1 do
+    for i = 1, 10000, 1 do
         numbers2[#numbers2 + 1] = math.random()
     end
 
@@ -240,8 +240,18 @@ function test18()
     math.randomseed(8675309, 1492101)
     good = good and math.random() ~= numbers1[i]
 
-    for i=1,10000,1 do
-        good = good and numbers1[i] == numbers2[i]
+    for i = 1, 10000, 1 do
+        numbers3[#numbers3 + 1] = math.random()
+    end
+
+    local numbers4 = {}
+    math.randomseed(2345, 1492101)
+    for i = 1, 10000, 1 do
+        numbers4[#numbers4 + 1] = math.random()
+    end
+
+    for i = 1, 10000, 1 do
+        good = good and numbers3[i] == numbers4[i]
     end
 
     -- `math.random(0)` should return a fully random integer.
@@ -270,47 +280,47 @@ end
 
 function test19()
     return math.sin(0) == 0.0 and
-           math.abs(math.sin(math.pi) - 0.0) < 1e-7 and
-           math.abs(math.sin(math.pi/6) - 0.5) < 1e-7 and
-           math.abs(math.sin(1) - 0.8414709848079) < 1e-7 and
-           is_nan(math.sin(math.huge)) and
-       not is_integer(math.sin(0))
+        math.abs(math.sin(math.pi) - 0.0) < 1e-7 and
+        math.abs(math.sin(math.pi / 6) - 0.5) < 1e-7 and
+        math.abs(math.sin(1) - 0.8414709848079) < 1e-7 and
+        is_nan(math.sin(math.huge)) and
+        not is_integer(math.sin(0))
 end
 
 function test20()
     return math.abs(math.sqrt(1.0) - 1.0) < 1e-7 and
-           math.abs(math.sqrt(4.0) - 2.0) < 1e-7 and
-           math.abs(math.sqrt(0.09) - 0.3) < 1e-7 and
-           is_nan(math.sqrt(-3))
+        math.abs(math.sqrt(4.0) - 2.0) < 1e-7 and
+        math.abs(math.sqrt(0.09) - 0.3) < 1e-7 and
+        is_nan(math.sqrt(-3))
 end
 
 function test21()
     return math.tan(0) == 0.0 and
-           math.abs(math.tan(math.pi) - 0.0) < 1e-7 and
-           math.abs(math.tan(math.pi/6) - 0.57735026918963) < 1e-7 and
-           math.abs(math.tan(1) - 1.5574077246549) < 1e-7 and
-           is_nan(math.tan(math.huge)) and
-       not is_integer(math.tan(0))
+        math.abs(math.tan(math.pi) - 0.0) < 1e-7 and
+        math.abs(math.tan(math.pi / 6) - 0.57735026918963) < 1e-7 and
+        math.abs(math.tan(1) - 1.5574077246549) < 1e-7 and
+        is_nan(math.tan(math.huge)) and
+        not is_integer(math.tan(0))
 end
 
 function test22()
     return math.tointeger(1.0) == 1 and
-           math.tointeger(1.1) == nil and
-           math.tointeger(-3.0) == -3 and
-           math.tointeger(4.00000002) == nil and
-           is_integer(math.tointeger(8.0))
+        math.tointeger(1.1) == nil and
+        math.tointeger(-3.0) == -3 and
+        math.tointeger(4.00000002) == nil and
+        is_integer(math.tointeger(8.0))
 end
 
 function test23()
     return math.type(1) == "integer" and
-           math.type(1.0) == "float" and
-           math.type("1.0") == nil
+        math.type(1.0) == "float" and
+        math.type("1.0") == nil
 end
 
 function test24()
     return not math.ult(-3, 2) and
-               math.ult(-3, -2) and
-               math.ult(1, 2)
+        math.ult(-3, -2) and
+        math.ult(1, 2)
 end
 
 assert(
